0e0f9add00f493f1a74b03698c4d06b747088aca
AntoninSevera <115144138+AntoninSevera@users.noreply.github.com>
2023-03-01 22:06:42 +0100

    almost finish

--- main.ts
+++ main.ts
@@ -21,11 +21,35 @@ basic.forever(function() {
     console.logValue("y", y)
     console.logValue("z", z)
     if (x > ceil || x < floor) {
-        basic.showNumber(4)
+        basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    } else if (x > ceil - 25 || x < floor + 25) {
+        music.playTone(Note.A, music.beat(BeatFraction.Quarter))
+    } else if (x > ceil - 50 || x < floor + 50) {
+        music.playTone(Note.A, music.beat(BeatFraction.Half))
+    } else if (x > ceil - 100 || x < floor + 100) {
+        music.playTone(Note.A, music.beat(BeatFraction.Whole))
     }
-    if (y > ceil || y < floor) {
-        basic.showNumber(5)
-    }
+    if (y > ceil || y < ceil) {
+        basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    } else if (y > ceil - 25 || y < floor + 25) {
+        music.playTone(Note.A, music.beat(BeatFraction.Quarter))
+    } else if (y > ceil - 50 || y < floor + 50) {
+        music.playTone(Note.A, music.beat(BeatFraction.Half))
+    } else if (y > ceil - 100 || y < floor + 100) {
+        music.playTone(Note.A, music.beat(BeatFraction.Whole))
+    } 
     basic.clearScreen()
     basic.pause(100)
 })
